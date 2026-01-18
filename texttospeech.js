6d37938b735f3dad2c6f75812cc5339c8991a174
Aaditya Karn <aadityakarn922@gmail.com>
2026-01-18 12:00:49 +0545

    Update texttospeech.js

--- texttospeech.js
+++ texttospeech.js
@@ -1,7 +1,7 @@
 let voiceselect = document.querySelector("select");
 let voices = [];
 
-// Load voices
+
 function populateVoices() {
     voices = window.speechSynthesis.getVoices();
     voiceselect.innerHTML = "";
@@ -12,25 +12,26 @@ function populateVoices() {
     });
 }
 
-// Some browsers load voices asynchronously
+
 window.speechSynthesis.onvoiceschanged = populateVoices;
 
-// Listen button
+
 document.querySelector("button").addEventListener("click", () => {
     const text = document.querySelector("textarea").value.trim();
     if (!text) return;
 
     let speech = new SpeechSynthesisUtterance(text);
 
-    // Set selected voice
+    
     const selectedIndex = voiceselect.value;
     if (voices[selectedIndex]) speech.voice = voices[selectedIndex];
 
     window.speechSynthesis.speak(speech);
 });
 
-// Change voice immediately when selected (optional)
+
 voiceselect.addEventListener("change", () => {
     const selectedIndex = voiceselect.value;
-    // You can optionally set default voice for next utterances
+    
 });
+
